9b7966b7209c6206108799ebb107f22d8bd47e9e
Cort Schaefer <cort@eschaefer.net>
2021-02-27 17:17:47 -0700

    add initial migration

---
+++ generate-sql.ps1
@@ -0,0 +1,60 @@
+$repo = "Cortside.WebApiStarter"
+$project = "src/$repo.Data"
+$startup = "src/$repo.WebApi"
+$context = "DatabaseContext"
+
+echo "Generating transactional SQL migrations for $project..."
+
+$begin = @" 
+PRINT 'Before TRY'
+BEGIN TRY
+	BEGIN TRAN
+	PRINT 'First Statement in the TRY block'
+
+"@
+
+$end = @" 
+	PRINT 'Last Statement in the TRY block'
+	COMMIT TRAN
+END TRY
+BEGIN CATCH
+    PRINT 'In CATCH Block'
+    IF(@@TRANCOUNT > 0)
+        ROLLBACK TRAN;
+
+    THROW; -- Raise error to the client.
+END CATCH
+PRINT 'After END CATCH'
+GO
+"@
+
+## get list of migrations
+$migrations = (dotnet ef migrations list --project "$project" --startup-project "$startup" --context "$context")
+
+$previousMigration="0"
+foreach($migration in $migrations) {
+	echo "Generating migration $migration..."
+	
+	$exists = test-path src/sql/$migration.migration.sql
+	if (!$exists) {
+	## generate single migration
+	dotnet ef migrations script $previousMigration $migration --idempotent --project "$project" --startup-project "$startup" --context "$context" --output src/sql/$migration.migration.sql
+
+	$exists = test-path src/sql/$migration.migration.sql
+	if ($exists) {
+		## read output file, replace GO statments and prepend and append transaction syntax
+		$text = Get-Content src/sql/$migration.migration.sql -Raw 
+		$text = $text.replace("GO`r`n","")
+		"$begin$text$end" | Out-File src/sql/$migration.migration.sql -Encoding UTF8
+	} else {
+		echo "error generating migration"
+	}
+	} else {
+	  echo "skipping, file already exists"
+	}
+	
+	## set last for next loop
+	$previousMigration=$migration 
+}
+
+echo "done"
--- src/Cortside.WebApiStarter.Data/Migrations/20210228001015_initial.Designer.cs
+++ src/Cortside.WebApiStarter.Data/Migrations/20210228001015_initial.Designer.cs
@@ -4,14 +4,16 @@ using Cortside.WebApiStarter.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cortside.WebApiStarter.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210228001015_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
